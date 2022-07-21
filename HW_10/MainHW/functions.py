9964de0e4b6a611b50c063b6cfcca62a7edf108b
InnaKo4 <gazetdinova-inna@mail.ru>
2022-07-21 16:13:56 +0500

    file edited

--- HW_10/MainHW/functions.py
+++ HW_10/MainHW/functions.py
@@ -1,8 +1,8 @@
 # полезные функции
 import json
 
-
 def load_candidates():
+    """Загружает данные из Json в Python"""
     with open("candidates.json", "r") as file:
         file_json = file.read()
         all_candidates = json.loads(file_json)
@@ -10,28 +10,31 @@ def load_candidates():
 
 
 def get_all(candidates):
+    """Получает весь список кандидатов в виде списка"""
     new_candidates = []
     for candidate in candidates:
         name = candidate['name']
         position = candidate['position']
         skills = candidate['skills']
-        candidate_information = f'<pre>\n{name}\n{position}\n{skills}'
+        candidate_information = f'<pre>\n{name}\n{position}\n{skills}\n'
         new_candidates.append(candidate_information)
     return new_candidates
 
 
 def get_by_pk(pk):
+    """Получает данные кандидата по его номеру"""
     candidates = load_candidates()
     for candidate in candidates:
         if candidate['pk'] == pk:
             return candidate
 
 def get_by_skill(skill_name):
+    """Получает данные кандидатов по навыку и выводит в виде списка"""
     skill_owned = []
     candidates = load_candidates()
     for candidate in candidates:
-        new_skills = candidate['skills'].lower()
-        if skill_name in new_skills:
+        candidates_skills = candidate['skills'].lower().split(',')
+        if skill_name.lower() in candidates_skills:
             name = candidate['name']
             position = candidate['position']
             skills = candidate['skills']
